48fa906eeeabb3386f90865afa4c3bf9f0259bbf
Elizabeth Merrill <eamerri2@asu.edu>
2023-10-22 00:59:34 -0600

    started working on linking all code together

---
+++ script.js
@@ -0,0 +1,41 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+
+
+
+// var https = require('https')
+// var fs = require('fs')
+
+// const PORT = aquariapp.com
+
+// fs.readFile('./index.html', function(error, html){
+//     if (error) throw error;
+//     https.createServer(function(request, response){
+//         response.writeHeader(200, {"Content-Type": "text/html"});
+//         response.write(html);
+//         response.end();
+// }).listen(PORT)
+// });
+
+// const navBtn = document.querySelector('.c-nav-btn')
+
+// navBtn.addEventListener("click", function(){
+//   navBtn.classList.toggle("is-active")
+// })
