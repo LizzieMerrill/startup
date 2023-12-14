3951af231bb3d426e484965fd465211063ebfafc
Elizabeth Merrill <eamerri2@asu.edu>
2023-12-14 16:32:17 -0700

    index.jsx work

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';//import ReactDOM from 'react-dom/client';
 import App from './src/app';
 
 // const React = require('react');
