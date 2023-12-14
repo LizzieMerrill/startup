f5072e39e62d70f20d402a56e2b2b056a9b730f4
Elizabeth Merrill <eamerri2@asu.edu>
2023-12-14 16:01:37 -0700

    babel config

--- index.jsx
+++ index.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './src/app';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './src/app';
+
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+const App = require('./src/app');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
